ef966164ab7591d11bb5603c899b94661f5dc35d
EllaKamenetsky <101578120+EllaKamenetsky@users.noreply.github.com>
2022-03-15 10:18:48 +0200

    Add files via upload
    files is uploaded

---
+++ UIAdmin/Model/UserFullInfo.cs
@@ -0,0 +1,16 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIAdmin.Model
+{
+    public class UserFullInfo
+    {
+        public UserInfo UserInfo { get; set; }
+        public List<string> PotentialSites { get; set; }
+        public List<string> AllSites { get; set; }
+    }
+}
